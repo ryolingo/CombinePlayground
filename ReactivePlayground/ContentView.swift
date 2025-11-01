2a8ae946fcd9e86c9fb50837e3f8b2e5096f3303
ryolingo <ryomatsuya04@gmail.com>
2025-11-01 15:09:04 +0900

    Initial Commit

---
+++ ReactivePlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReactivePlayground
+//
+//  Created by matsumotoryota on 2025/11/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReactivePlayground/ReactivePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactivePlaygroundApp.swift
+//  ReactivePlayground
+//
+//  Created by matsumotoryota on 2025/11/01.
+//
+
+import SwiftUI
+
+@main
+struct ReactivePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
